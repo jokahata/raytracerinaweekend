e759a89320fa8476caf2d637340cad4ef9049b6f
John Okahata <jokahata@users.noreply.github.com>
2019-02-03 14:53:16 -0800

    Initial commit

---
+++ raytracyer/vec3.hpp
@@ -0,0 +1,15 @@
+//
+//  vec3.hpp
+//  raytracyer
+//
+//  Created by John Okahata on 2/3/19.
+//  Copyright © 2019 John Okahata. All rights reserved.
+//
+
+#ifndef vec3_hpp
+#define vec3_hpp
+
+#include <stdio.h>
+
+#endif /* vec3_hpp */
+
